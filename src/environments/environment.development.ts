3d493bb53c5e44ad322a8453901f6b1dc23e2075
Saber-khalfallah <sabeur.khalfallah94@gmail.com>
2025-01-20 20:17:55 +0100

    FireBase config update

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,12 +2,12 @@
 export const environment = {
     production : false,
     firebase : {
-        apiKey: "AIzaSyBPm22zpGTPg8ypPWT1QcsrkC9zi8uVvmQ",
-        authDomain: "films-b2553.firebaseapp.com",
-        projectId: "films-b2553",
-        storageBucket: "films-b2553.appspot.com",
-        messagingSenderId: "773758281669",
-        appId: "1:773758281669:web:37de9cdc3b2dc6e07dcdac"
-      }
+      apiKey: "AIzaSyBPm22zpGTPg8ypPWT1QcsrkC9zi8uVvmQ",
+      authDomain: "films-b2553.firebaseapp.com",
+      projectId: "films-b2553",
+      storageBucket: "films-b2553.firebasestorage.app",
+      messagingSenderId: "773758281669",
+      appId: "1:773758281669:web:37de9cdc3b2dc6e07dcdac"
+    }
     };
 
